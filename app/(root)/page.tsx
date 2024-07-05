f9d34d94844dafebd7881ce0d6df6d82bc22649f
atharv0004 <rupapatil781@gmail.com>
2024-07-05 23:24:16 +0530

    implement right and left sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,41 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import RightSidebar from '@/components/RightSidebar';
 
 const Home  = () => {
+
+  const loggedIn = { firstName: 'Atharv' , lastName: 'Patil'
+    , email: 'atharvpatil781@gmail.com'
+  };
+
   return (
-    <div>Home </div>
+    <section className="home">Home 
+    <div className='home-content'>
+      <header className='home-header'>
+       <HeaderBox 
+       type="greeting"
+       title="Welcome"
+       user={loggedIn?.firstName || 'Guest'}
+       subtext="Access and manage your account and transactions efficently"
+       />
+
+
+     <TotalBalanceBox
+        accounts={[]}
+        totalBanks={1}
+        totalCurrentBalance={1250.35}
+        />
+      </header>
+
+      recent transactions
+    </div>
+
+    <RightSidebar user={loggedIn}
+                transactions={[]}
+                banks={[{ currentBalance: 123.50},
+                {currentBalance: 123.50}]} />
+    </section>
   )
 }
 
